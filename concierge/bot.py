ad23f0f20526cf1bb0f68a03a0fd5fb8c1fe0b9b
taven <8206808+daegontaven@users.noreply.github.com>
2021-05-16 16:18:09 +0530

    Show discriminator

--- concierge/bot.py
+++ concierge/bot.py
@@ -44,7 +44,7 @@ class ConciergeBot(commands.Bot):
                         f"please ensure you've read and understood "
                         f"the {rules_tc.mention} to make your stay worthwhile."
         )
-        embed.set_footer(text=member.name, icon_url=member.avatar_url)
+        embed.set_footer(text=f"{member.name}#{member.discriminator}", icon_url=member.avatar_url)
         member_role = discord.utils.get(guild.roles, name="Member")
         if member_role in member.roles:
             await general_tc.send(embed=embed)
@@ -74,7 +74,7 @@ async def on_webhook_received(data):
                         f"please ensure you've read and understood "
                         f"the {rules_tc.mention} to make your stay worthwhile."
         )
-        embed.set_footer(text=member.name, icon_url=member.avatar_url)
+        embed.set_footer(text=f"{member.name}#{member.discriminator}", icon_url=member.avatar_url)
         await general_tc.send(embed=embed)
     elif request['OP'] == 5:
         logger.info(f"Test Webhook: {request}")
